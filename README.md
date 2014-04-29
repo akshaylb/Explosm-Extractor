c096df88b06a9c6710b23b2b758e148676ba896d
akshaylb <akshay.ksth@gmail.com>
2014-04-29 13:00:16 +0545

    Removed redundant page read and use of findall, cleaned up script

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-## Explosm-Extractor
-
-Contains a basic python script to download all Cyanide and Happiness comics.
-
----
-
-### How To Use
-
-```bash
-$ python2.7 extract.py
-```
+### Explosm-Extractor
 ===
-
-### Issues
-######This script is hella slow. 
-*And before you mention it, I know I could have used wget and curl for the same purpose!*
+Contains a basic python script to download all Cyanide and Happiness comics.
+---
+##### Dependencies
+[Mechanize](https://pypi.python.org/pypi/mechanize/)
+##### How To Use
+Just type `python2.7 extract.py` or `python extract.py` based on your distro.
+##### Note
++ One could have used wget and curl to achieve the same purpose, but I didnt want to.
++ Why **Mechanise** and just not plain urllib?
+...Clean API. Although could work without it.
++ Why not use **BeautifulSoup**? 
+...Too much API. 
--- extract.py
+++ extract.py
@@ -1,23 +1,28 @@
 import urllib, mechanize, re, os
 br=mechanize.Browser()
-os.makedirs('C&HComics')
-os.chdir('C&HComics')
+GLOBALDIR='C&HComics'
+if not os.path.exists(GLOBALDIR):
+	os.makedirs(GLOBALDIR)
+os.chdir(GLOBALDIR)
 def getcomic(path):
-    source=urllib.urlopen(path).read()
-    for m in re.findall('http://www.explosm.net/db/files/Comics/[-\w/]+\.(?:jpg|gif|png)', source):
-		print m
-		parts=m.split('/')
+    print "Reading link "+path+" ..."
+    source=br.open(path).read()
+    imgpath=re.search('http://www.explosm.net/db/files/Comics/[-\w/]+\.(?:jpg|gif|png)', source).group(0)
+    if imgpath:
+		print "Found image path "+imgpath
+		parts=imgpath.split('/')
 		if not parts[-2]=='Comics':
 			if not os.path.exists(parts[-2]):
 				os.makedirs(parts[-2])
-		file=urllib.urlopen(m).read()
-		outputfile=open(parts[-2]+'/'+parts[-1],'wb')
-		outputfile.write(file)
-		outputfile.close
-    br.open(path)
+		file=urllib.urlopen(imgpath).read()
+		pathname=parts[-2]+'/'+parts[-1]
+		if not os.path.exists(pathname):
+			outputfile=open(pathname,'wb')
+			outputfile.write(file)
+			outputfile.close
     rs=br.follow_link(text_regex=r"Previous")
-    print rs.geturl()
+    print "Found next link "+rs.geturl()
     getcomic(rs.geturl())
     return
 
-getcomic("http://explosm.net/comics/3535/")	
+getcomic("http://explosm.net/comics")	
